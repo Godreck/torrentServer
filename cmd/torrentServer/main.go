1a864273cc48beac8ac8da3a174bb9698c1f8d49
Арсений Веремьёв <bedrar@MacBook-Pro-Arsenij.local>
2025-05-20 14:01:14 +0300

    Добавил logger setup и начал писать сам логгер

--- cmd/torrentServer/main.go
+++ cmd/torrentServer/main.go
@@ -1,15 +1,47 @@
+// cmd/torrentServer
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 	"torrentServer/http_server/handlers/search"
+	"torrentServer/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
+	// загружаем конфиг
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env)) // к каждому сообщению будет добавляться поле с информацией о текущем окружении
+
+	log.Info("initializing server", slog.String("address", cfg.Address)) // Помимо сообщения выведем параметр с адресом
+	log.Debug("logger debug mode enabled")
+
 	http.HandleFunc("/search", search.SearchHandler)
-	log.Println("Server is running on port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+		log.Error(err.Error())
 	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
